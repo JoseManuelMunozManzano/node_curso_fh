92bbc975400a4177ca6c825514162cfdcf8a6c80
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-06-10 18:13:29 +0200

    3-Tabla del 5

---
+++ 03-bases-node/index.js
@@ -0,0 +1,15 @@
+// 5 x 1 = 5
+// 5 x 2 = 10
+// ...
+// 5 x 10 = 50
+
+// Para limpiar la consola automáticamente antes de mostrar el resultado.
+console.clear();
+
+console.log('=================');
+console.log('   TABLA DEL 5   ');
+console.log('=================');
+
+for (let i = 1; i < 11; i++) {
+  console.log(`5 x ${i} = ${5 * i}`);
+}
