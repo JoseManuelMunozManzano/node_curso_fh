9fa0a7739850f2ffdf0b9eaf7dd6d3097610ae92
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-06-10 06:07:36 +0200

    2-Función autoinvocada

--- 02-fundamentos_js/const-var-let.js
+++ 02-fundamentos_js/const-var-let.js
@@ -1,53 +1,54 @@
 // Simple resumen de const vs let vs var
+(() => {
+  console.log('VAR vs LET vs CONST');
 
-console.log('VAR vs LET vs CONST');
+  // ----------------------------- var -----------------------------
+  // De las versiones antiguas de JS. No se recomienda su uso porque da lugar a errores y usos extraños.
+  // Se crea la variable en un ámbito global.
+  var nombre = 'Wolverine';
 
-// ----------------------------- var -----------------------------
-// De las versiones antiguas de JS. No se recomienda su uso porque da lugar a errores y usos extraños.
-// Se crea la variable en un ámbito global.
-var nombre = 'Wolverine';
+  if (true) {
+    // Esta variable debería ser local a este if
+    // PERO como nombre se creó con var, la variable nombre es de AMBITO GLOBAL y SALE DEL if.
+    var nombre = 'Magneto';
+  }
 
-if (true) {
-  // Esta variable debería ser local a este if
-  // PERO como nombre se creó con var, la variable nombre es de AMBITO GLOBAL y SALE DEL if.
-  var nombre = 'Magneto';
-}
+  // Aquí nombre tiene el valor Magneto.
+  console.log('Nombre:', nombre);
 
-// Aquí nombre tiene el valor Magneto.
-console.log('Nombre:', nombre);
+  // ----------------------------- let -----------------------------
+  // Usar let cuando estamos seguros de que nuestras variables van a cambiar sus valores.
+  // Variable declarada a nivel global.
+  let nombre2 = 'Wolverine';
 
-// ----------------------------- let -----------------------------
-// Usar let cuando estamos seguros de que nuestras variables van a cambiar sus valores.
-// Variable declarada a nivel global.
-let nombre2 = 'Wolverine';
+  if (true) {
+    // Esta variable es declarada a nivel local en este if y NO SALE del if.
+    // Es otro scope distinto.
+    let nombre2 = 'Magneto';
 
-if (true) {
-  // Esta variable es declarada a nivel local en este if y NO SALE del if.
-  // Es otro scope distinto.
-  let nombre2 = 'Magneto';
+    // NOTA: Si en vez de declarar la variable de nuevo se quisiera ejecutar:
+    // nombre2 = 'Magneto';
+    // entonces, como no hay declaración, se busca la declaración en un scope superior
+    // y SI cambia el valor de Wolverine a Magneto.
+  }
 
-  // NOTA: Si en vez de declarar la variable de nuevo se quisiera ejecutar:
-  // nombre2 = 'Magneto';
-  // entonces, como no hay declaración, se busca la declaración en un scope superior
-  // y SI cambia el valor de Wolverine a Magneto.
-}
+  // Aquí nombre tiene el valor Wolverine.
+  console.log('Nombre2:', nombre2);
 
-// Aquí nombre tiene el valor Wolverine.
-console.log('Nombre2:', nombre2);
+  // ----------------------------- const -----------------------------
+  // Dentro de lo posible, es mejor declarar todo como constantes, es más rápido y menos proclive a errores.
+  // Constante declarada a nivel global.
+  const nombre3 = 'Wolverine';
 
-// ----------------------------- const -----------------------------
-// Dentro de lo posible, es mejor declarar todo como constantes, es más rápido y menos proclive a errores.
-// Constante declarada a nivel global.
-const nombre3 = 'Wolverine';
+  if (true) {
+    // Esta constante es declarada a nivel local en este if y NO SALE del if.
+    // Es otro scope distinto.
+    const nombre3 = 'Magneto';
 
-if (true) {
-  // Esta constante es declarada a nivel local en este if y NO SALE del if.
-  // Es otro scope distinto.
-  const nombre3 = 'Magneto';
+    // NOTA: Esto no es posible, da error, porque nombre3 es una CONSTANTE
+    // nombre3 = 'Magneto';
+  }
 
-  // NOTA: Esto no es posible, da error, porque nombre3 es una CONSTANTE
-  // nombre3 = 'Magneto';
-}
-
-// Aquí nombre tiene el valor Wolverine.
-console.log('Nombre3:', nombre3);
+  // Aquí nombre tiene el valor Wolverine.
+  console.log('Nombre3:', nombre3);
+})();
