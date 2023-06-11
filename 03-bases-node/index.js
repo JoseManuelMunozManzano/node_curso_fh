ea646e39278646104bac9ceb59189c472d4f171b
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-06-11 07:01:32 +0200

    3-Recibir información desde línea de comandos

--- 03-bases-node/index.js
+++ 03-bases-node/index.js
@@ -27,13 +27,23 @@ console.clear();
 // ];
 //
 // Por tanto, nuestros argumentos siempre comienzan en la posición 2 del array.
-console.log(process.argv);
-// Usando la desestructuración obtenemos los argumentos que me interesan.
-const [, , arg3] = process.argv;
+
+// Ahora, usando desestructuración, obtenemos ese tercer elemento del array, y de el, el valor numérico de base.
+//! ESTA FORMA ES PARA VERLO RAPIDO, CON FINES EDUCATIVOS, PERO TIENE MUCHOS PROBLEMAS. NO HACERLO ASI NUNCA!!!
+// En concreto, un problema sería que los argumentos los estamos tratando por posición, y en esa posición
+// puede que me envíen otro argumento en vez de base, si el programa admite más de un argumento,
+// y lo va a tratar como la base.
+// Otro problema es que podemos tener nombres cortos para el argumento o no indicar el igual.
+// Por ejemplo nos valdrían: --base=5, -b=5 y --base 5
+//
+// Y otro añadido importante sería la posibilidad de tener esa documentación, cómo se puede usar nuestra aplicación.
+//
+// Ya existe un paquete para manejar todo eso, llamado yargs
+const [, , arg3 = 'base=5'] = process.argv;
+const [, base = 5] = arg3.split('=');
 
 //? Se va a imprimir la tabla de multiplicar y grabarla en un archivo de texto que esté en la
 //? misma dirección de mi aplicación.
-// const base = 5;
-// crearArchivo(base)
-//   .then((nombreArchivo) => console.log(nombreArchivo, 'creado'))
-//   .catch((err) => console.log(err));
+crearArchivo(base)
+  .then((nombreArchivo) => console.log(nombreArchivo, 'creado'))
+  .catch((err) => console.log(err));
