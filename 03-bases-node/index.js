57236bd2388b8c6e2f8df21cc514124d7ffb7b1e
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-06-11 07:56:30 +0200

    3-yargs primer vistazo

--- 03-bases-node/index.js
+++ 03-bases-node/index.js
@@ -1,6 +1,7 @@
-// Dejamos el punto de inicio de la aplicación lo más vacío posible, llevándonos la lógica
-// a otros fuentes e importando lo necesario.
 import { crearArchivo } from './helpers/multiplicar.js';
+// https://www.npmjs.com/package/yargs
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 
 // Para limpiar la consola automáticamente antes de mostrar el resultado.
 console.clear();
@@ -27,23 +28,46 @@ console.clear();
 // ];
 //
 // Por tanto, nuestros argumentos siempre comienzan en la posición 2 del array.
+console.log(process.argv);
 
-// Ahora, usando desestructuración, obtenemos ese tercer elemento del array, y de el, el valor numérico de base.
-//! ESTA FORMA ES PARA VERLO RAPIDO, CON FINES EDUCATIVOS, PERO TIENE MUCHOS PROBLEMAS. NO HACERLO ASI NUNCA!!!
-// En concreto, un problema sería que los argumentos los estamos tratando por posición, y en esa posición
-// puede que me envíen otro argumento en vez de base, si el programa admite más de un argumento,
-// y lo va a tratar como la base.
-// Otro problema es que podemos tener nombres cortos para el argumento o no indicar el igual.
-// Por ejemplo nos valdrían: --base=5, -b=5 y --base 5
+// Usando yargs
 //
-// Y otro añadido importante sería la posibilidad de tener esa documentación, cómo se puede usar nuestra aplicación.
+// Si ejecutamos la siguiente llamada:
+//      npm start
+// Nos devuelve el siguiente objeto
 //
-// Ya existe un paquete para manejar todo eso, llamado yargs
-const [, , arg3 = 'base=5'] = process.argv;
-const [, base = 5] = arg3.split('=');
+// { _: [], '$0': 'index.js' }
+//
+// Si ejecutamos la siguiente llamada:
+//     npm start -- --base=9
+// El objeto devuelto es este:
+//
+// { _: [], base: 9, '$0': 'index.js' }
+//
+// Otra llamada sería:
+//    npm start --base 9 --listar
+// Que devuelve:
+// { _: [], base: 9, listar: true, '$0': 'index.js' }
+//
+// Otra llamada sería:
+//    npm start --base 9 -l
+// Que devuelve:
+// { _: [], base: 9, l: true, '$0': 'index.js' }
+//
+// También, gracias a yargs, tenemos la ayuda:
+//      npm run node -- --help
+// Y la versión de package.json
+//      npm run node -- --version
+const argv = yargs(hideBin(process.argv)).argv;
+console.log(argv);
+// Y para obtener un argumento mandado por consola, es muy fácil, se obtiene por su nombre:
+// Lo bueno es que si hacemos la llamada:
+//      npm start -- --base 9
+// Funciona correctamente, devolviendo el valor 9
+console.log('base: yargs', argv.base);
 
 //? Se va a imprimir la tabla de multiplicar y grabarla en un archivo de texto que esté en la
 //? misma dirección de mi aplicación.
-crearArchivo(base)
-  .then((nombreArchivo) => console.log(nombreArchivo, 'creado'))
-  .catch((err) => console.log(err));
+// crearArchivo(base)
+//   .then((nombreArchivo) => console.log(nombreArchivo, 'creado'))
+//   .catch((err) => console.log(err));
