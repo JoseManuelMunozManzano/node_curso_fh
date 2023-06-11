d7ba7b47b0a65dadfb3402eaeea42087a2fd3f56
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-06-11 13:10:07 +0200

    4-Creación función asíncrona

--- 04-lista-tareas/index.js
+++ 04-lista-tareas/index.js
@@ -1,1 +1,11 @@
 import 'colors';
+
+console.clear();
+
+// Vamos a trabajar con procesos asíncronos, con async-await.
+// Para poder usar await (antes, ahora ya es posible) vamos a crear una función async
+const main = async () => {
+  console.log('Hola Mundo');
+};
+
+main();
