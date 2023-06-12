64c27f3e64486f02e29a2b2594fa1ef88cc9c03d
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-06-12 07:13:10 +0200

    4-Promesas - Repetir menú de forma infinita

--- 04-lista-tareas/helpers/mensajes.js
+++ 04-lista-tareas/helpers/mensajes.js
@@ -2,44 +2,50 @@ import { createInterface } from 'readline';
 import 'colors';
 
 export const mostrarMenu = () => {
-  console.clear();
-  console.log('=========================='.green);
-  console.log('  Seleccione una opción'.green);
-  console.log('==========================\n'.green);
+  return new Promise((resolve) => {
+    console.clear();
+    console.log('=========================='.green);
+    console.log('  Seleccione una opción'.green);
+    console.log('==========================\n'.green);
 
-  console.log(`${'1.'.green} Crear tarea`);
-  console.log(`${'2.'.green} Listar tareas`);
-  console.log(`${'3.'.green} Listar tareas completadas`);
-  console.log(`${'4.'.green} Listar tareas pendientes`);
-  console.log(`${'5.'.green} Completar tarea(s)`);
-  console.log(`${'6.'.green} Borrar tarea`);
-  console.log(`${'0.'.green} Salir\n`);
+    console.log(`${'1.'.green} Crear tarea`);
+    console.log(`${'2.'.green} Listar tareas`);
+    console.log(`${'3.'.green} Listar tareas completadas`);
+    console.log(`${'4.'.green} Listar tareas pendientes`);
+    console.log(`${'5.'.green} Completar tarea(s)`);
+    console.log(`${'6.'.green} Borrar tarea`);
+    console.log(`${'0.'.green} Salir\n`);
 
-  // Recibir información del usuario.
-  const readline = createInterface({
-    // Con esto Node sabe que tiene que pausar la ejecución del programa, esperando que el usuario
-    // introduzca contenido y pulse Intro.
-    input: process.stdin,
-    // Para mostrar información en consola, cuando se le pide algo al usuario.
-    output: process.stdout,
-  });
+    // Recibir información del usuario.
+    const readline = createInterface({
+      // Con esto Node sabe que tiene que pausar la ejecución del programa, esperando que el usuario
+      // introduzca contenido y pulse Intro.
+      input: process.stdin,
+      // Para mostrar información en consola, cuando se le pide algo al usuario.
+      output: process.stdout,
+    });
 
-  // Para mostrar con stdout la información al usuario.
-  // Se ejecuta un callback cuando se termina de ejecutar el question, pasándole la opción
-  // que ha indicado el usuario.
-  readline.question('Seleccione una opción: ', (opt) => {
-    // Obligatorio cerrar cuando terminamos
-    readline.close();
+    // Para mostrar con stdout la información al usuario.
+    // Se ejecuta un callback cuando se termina de ejecutar el question, pasándole la opción
+    // que ha indicado el usuario.
+    readline.question('Seleccione una opción: ', (opt) => {
+      // Obligatorio cerrar cuando terminamos
+      readline.close();
+      resolve(opt);
+    });
   });
 };
 
 export const pausa = () => {
-  const readline = createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+  return new Promise((resolve) => {
+    const readline = createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-  readline.question(`\nPresione ${'ENTER'.green} para continuar\n`, (opt) => {
-    readline.close();
+    readline.question(`\nPresione ${'ENTER'.green} para continuar\n`, (opt) => {
+      readline.close();
+      resolve();
+    });
   });
 };
--- 04-lista-tareas/index.js
+++ 04-lista-tareas/index.js
@@ -7,15 +7,22 @@ console.clear();
 // Para poder usar await (antes, ahora ya es posible) vamos a crear una función async
 const main = async () => {
   console.log('Hola Mundo');
-  mostrarMenu();
 
-  // PROBLEMA: Cuando se ejecuta mostrarMenu(), ahí hay un callback y lo pone en la cola de callbacks.
-  //   En pausa() hay otro callback donde se crea otra instancia de readline y está tomando esta última,
-  //   haciendo que el primer callback no llegue a ejecutarse.
-  //   Tenemos que esperar a que la persona ingrese la opción en el callback de mostrarMenu ANTES de
-  //   ejecutar el callback de pausa()
-  //
-  // pausa();
+  let opt = '';
+  do {
+    opt = await mostrarMenu();
+
+    // PROBLEMA: Cuando se ejecuta mostrarMenu(), ahí hay un callback y lo pone en la cola de callbacks.
+    //   En pausa() hay otro callback donde se crea otra instancia de readline y está tomando esta última,
+    //   haciendo que el primer callback no llegue a ejecutarse.
+    //   Tenemos que esperar a que la persona ingrese la opción en el callback de mostrarMenu ANTES de
+    //   ejecutar el callback de pausa()
+    //
+    // SOLUCION: Promises!! tanto en mostrarMenu() para que me dé tiempo a escribir la opción como en pausa()
+    if (opt !== '0') {
+      await pausa();
+    }
+  } while (opt !== '0');
 };
 
 main();
