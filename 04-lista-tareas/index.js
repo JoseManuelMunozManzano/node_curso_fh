d2ee0ec0ec9d24bd2c29526c2d8804940e61cd50
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-06-12 06:58:14 +0200

    4-stdin - stdout - readline

--- 04-lista-tareas/index.js
+++ 04-lista-tareas/index.js
@@ -1,4 +1,5 @@
 import 'colors';
+import { mostrarMenu, pausa } from './helpers/mensajes.js';
 
 console.clear();
 
@@ -6,6 +7,15 @@ console.clear();
 // Para poder usar await (antes, ahora ya es posible) vamos a crear una función async
 const main = async () => {
   console.log('Hola Mundo');
+  mostrarMenu();
+
+  // PROBLEMA: Cuando se ejecuta mostrarMenu(), ahí hay un callback y lo pone en la cola de callbacks.
+  //   En pausa() hay otro callback donde se crea otra instancia de readline y está tomando esta última,
+  //   haciendo que el primer callback no llegue a ejecutarse.
+  //   Tenemos que esperar a que la persona ingrese la opción en el callback de mostrarMenu ANTES de
+  //   ejecutar el callback de pausa()
+  //
+  // pausa();
 };
 
 main();
