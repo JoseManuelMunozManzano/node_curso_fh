c93aee8e2bfb68a6911deba15ebc1aca6e7876b1
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-06-15 17:33:53 +0200

    5-Inicio de proyecto - ClimaApp

---
+++ 05-clima-app/helpers/inquirer.js
@@ -0,0 +1,158 @@
+// https://www.npmjs.com/package/inquirer
+import inquirer from 'inquirer';
+import 'colors';
+
+const preguntas = [
+  {
+    type: 'list',
+    name: 'opcion',
+    message: '¿Qué desea hacer?',
+    choices: [
+      {
+        value: '1',
+        name: `${'1.'.green} Crear tarea`,
+      },
+      {
+        value: '2',
+        name: `${'2.'.green} Listar tareas`,
+      },
+      {
+        value: '3',
+        name: `${'3.'.green} Listar tareas completadas`,
+      },
+      {
+        value: '4',
+        name: `${'4.'.green} Listar tareas pendientes`,
+      },
+      {
+        value: '5',
+        name: `${'5.'.green} Completar tarea(s)`,
+      },
+      {
+        value: '6',
+        name: `${'6.'.green} Borrar tarea`,
+      },
+      {
+        value: '0',
+        name: `${'0.'.green} Salir`,
+      },
+    ],
+  },
+];
+
+export const inquirerMenu = async () => {
+  console.clear();
+  console.log('=========================='.green);
+  console.log('  Seleccione una opción'.white);
+  console.log('==========================\n'.green);
+
+  const { opcion } = await inquirer.prompt(preguntas);
+
+  return opcion;
+};
+
+export const pausa = async () => {
+  const question = [
+    {
+      type: 'input',
+      name: 'enter',
+      message: `Presione ${'ENTER'.green} para continuar`,
+    },
+  ];
+
+  console.log('\n');
+  await inquirer.prompt(question);
+};
+
+export const leerInput = async (message) => {
+  const question = [
+    {
+      type: 'input',
+      name: 'desc',
+      message,
+
+      validate(value) {
+        if (value.length === 0) {
+          return 'Por favor ingrese un valor';
+        }
+        return true;
+      },
+    },
+  ];
+
+  const { desc } = await inquirer.prompt(question);
+  return desc;
+};
+
+export const listadoTareasBorrar = async (tareas = []) => {
+  const choices = tareas.map((tarea, i) => {
+    const idx = `${i + 1}.`.green;
+
+    return {
+      value: tarea.id,
+      name: `${idx} ${tarea.desc}`,
+    };
+  });
+
+  const preguntas = [
+    {
+      type: 'list',
+      name: 'id',
+      message: 'Borrar',
+      choices,
+      // Número de elementos que se ven. Con esto consigo evitar el error de loop al moverme con el cursor
+      // sobre las opciones.
+      pageSize: 10,
+      // loop: false,
+    },
+  ];
+
+  // Para añadir al principio de un arreglo
+  choices.unshift({
+    value: '0',
+    name: '0.'.green + ' Cancelar',
+  });
+
+  const { id } = await inquirer.prompt(preguntas);
+
+  return id;
+};
+
+export const confirmar = async (message = '') => {
+  const pregunta = [
+    {
+      type: 'confirm',
+      name: 'ok',
+      message,
+    },
+  ];
+
+  const { ok } = await inquirer.prompt(pregunta);
+
+  return ok;
+};
+
+export const mostrarListadoChecklist = async (tareas = []) => {
+  const choices = tareas.map((tarea, i) => {
+    const idx = `${i + 1}.`.green;
+
+    return {
+      value: tarea.id,
+      name: `${idx} ${tarea.desc}`,
+      checked: tarea.completadoEn ? true : false,
+    };
+  });
+
+  const pregunta = [
+    {
+      type: 'checkbox',
+      name: 'ids',
+      message: 'Selecciones',
+      choices,
+    },
+  ];
+
+  const { ids } = await inquirer.prompt(pregunta);
+
+  return ids;
+};
