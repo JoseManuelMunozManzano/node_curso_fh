12ef3536abb5c88f0d41cfc24e253ff16c3712cd
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-06-16 06:46:48 +0200

    5-Modelo para controlar la aplicación

--- 05-clima-app/index.js
+++ 05-clima-app/index.js
@@ -1,6 +1,8 @@
 import { inquirerMenu, leerInput, pausa } from './helpers/inquirer.js';
+import Busquedas from './models/busquedas.js';
 
 const main = async () => {
+  const busquedas = new Busquedas();
   let opt;
 
   do {
@@ -8,7 +10,24 @@ const main = async () => {
 
     switch (opt) {
       case 1:
-        console.log('Seleccionada opción 1');
+        // Mostrar mensaje
+        const lugar = await leerInput('Ciudad: ');
+        console.log(lugar);
+
+        // Buscar los lugares
+
+        // Seleccionar el lugar
+
+        // Clima
+
+        // Mostrar resultados
+        console.log('\nInformación de la ciudad\n'.green);
+        console.log('Ciudad:');
+        console.log('Lat:');
+        console.log('Lng:');
+        console.log('Temperatura:');
+        console.log('Mínima:');
+        console.log('Máxima:');
         break;
 
       case 2:
---
+++ 05-clima-app/models/busquedas.js
@@ -0,0 +1,15 @@
+export default class Busquedas {
+  historial = ['Tegucigalpa', 'Madrid', 'San José', 'Bogotá'];
+
+  constructor() {
+    // TODO: leer DB si existe
+  }
+
+  async ciudad(lugar = '') {
+    // petición http
+    console.log(lugar);
+
+    // retornar los lugares
+    return [];
+  }
+}
