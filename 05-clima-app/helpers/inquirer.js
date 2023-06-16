8235f4f039509bb5ca34238d619daa2d1f807fce
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-06-16 06:38:25 +0200

    5-Menú de la aplicación

--- 05-clima-app/helpers/inquirer.js
+++ 05-clima-app/helpers/inquirer.js
@@ -9,31 +9,15 @@ const preguntas = [
     message: '¿Qué desea hacer?',
     choices: [
       {
-        value: '1',
-        name: `${'1.'.green} Crear tarea`,
+        value: 1,
+        name: `${'1.'.green} Buscar ciudad`,
       },
       {
-        value: '2',
-        name: `${'2.'.green} Listar tareas`,
+        value: 2,
+        name: `${'2.'.green} Historial`,
       },
       {
-        value: '3',
-        name: `${'3.'.green} Listar tareas completadas`,
-      },
-      {
-        value: '4',
-        name: `${'4.'.green} Listar tareas pendientes`,
-      },
-      {
-        value: '5',
-        name: `${'5.'.green} Completar tarea(s)`,
-      },
-      {
-        value: '6',
-        name: `${'6.'.green} Borrar tarea`,
-      },
-      {
-        value: '0',
+        value: 0,
         name: `${'0.'.green} Salir`,
       },
     ],
--- 05-clima-app/index.js
+++ 05-clima-app/index.js
@@ -1,8 +1,23 @@
-import { leerInput } from './helpers/inquirer.js';
+import { inquirerMenu, leerInput, pausa } from './helpers/inquirer.js';
 
 const main = async () => {
-  const texto = await leerInput('Hola: ');
-  console.log(texto);
+  let opt;
+
+  do {
+    opt = await inquirerMenu();
+
+    switch (opt) {
+      case 1:
+        console.log('Seleccionada opción 1');
+        break;
+
+      case 2:
+        console.log('Seleccionada opción 2');
+        break;
+    }
+
+    if (opt !== 0) await pausa();
+  } while (opt !== 0);
 };
 
 main();
