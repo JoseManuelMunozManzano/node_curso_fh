e9cc9964daf622223f9128e19463a71f40c4b502
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-06-19 17:20:26 +0200

    9-Express y .env añadidos

--- 09-restserver/index.js
+++ 09-restserver/index.js
@@ -1,1 +1,13 @@
-console.log('Hola Mundo');
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
+app.listen(port, () => {
+  console.log('Servidor corriendo en puerto', port);
+});
