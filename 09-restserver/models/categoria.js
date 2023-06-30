71d54b6df1e825218771fc622609699b75f3beb5
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-06-30 18:42:10 +0200

    9-Modelo Categoria

---
+++ 09-restserver/models/categoria.js
@@ -0,0 +1,26 @@
+// Nombre del archivo categoria.js, en singular.
+
+import { Schema, model } from 'mongoose';
+
+const CategoriaSchema = Schema({
+  nombre: {
+    type: String,
+    required: [true, 'El nombre es obligatorio'],
+  },
+
+  estado: {
+    type: Boolean,
+    default: true,
+    required: true,
+  },
+
+  // El tipo es otro objeto de Mongo e indicamos el esquema 'Usuario'. Ese texto tiene que ser
+  // el mismo que aparece en usuario.js, al final en el model.
+  usuario: {
+    type: Schema.Types.ObjectId,
+    ref: 'Usuario',
+    required: true,
+  },
+});
+
+export const Categoria = model('Categoria', CategoriaSchema);
---
+++ 09-restserver/models/index.js
@@ -0,0 +1,4 @@
+export { Categoria } from './categoria';
+export { Role } from './role';
+export { Server } from './server';
+export { Usuario } from './usuario';
