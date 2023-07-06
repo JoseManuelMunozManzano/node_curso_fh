488cd779be3d042f093e9d78d6e95e492a9f71ff
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-07-06 06:47:02 +0200

    11-Mensajes de conexión y desconexión - Cliente

--- 11-websocket-server/public/js/socket-client.js
+++ 11-websocket-server/public/js/socket-client.js
@@ -1,4 +1,26 @@
+// Referencias del HTML
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+
 // Aquí estará toda la configuración y comunicación que vamos a tener, usando WebSockets, con el servidor.
 
 // Para ver desde el servidor el cliente conectado.
 const socket = io();
+
+// Listeners: son observables que están escuchando cambios o eventos
+
+// Cuando me conecto al servidor
+socket.on('connect', () => {
+  console.log('Conectado');
+
+  lblOffline.style.display = 'none';
+  lblOnline.style.display = '';
+});
+
+// Cuando me desconecto del servidor
+socket.on('disconnect', () => {
+  console.log('Desconectado del servidor');
+
+  lblOnline.style.display = 'none';
+  lblOffline.style.display = '';
+});
