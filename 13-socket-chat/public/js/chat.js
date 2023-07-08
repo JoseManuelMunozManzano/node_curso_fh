d7dd7f668c39874d3a36135b063e2ffe515bea96
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-07-08 17:17:41 +0200

    13-Mostrar en el HTML los usuarios conectados

--- 13-socket-chat/public/js/chat.js
+++ 13-socket-chat/public/js/chat.js
@@ -59,12 +59,26 @@ const conectarSocket = async () => {
     // TODO:
   });
 
-  socket.on('usuarios-activos', (payload) => {
-    console.log(payload);
-  });
+  socket.on('usuarios-activos', dibujarUsuarios);
 
   socket.on('mensaje-privado', () => {
     // TODO:
+  });
+};
+
+const dibujarUsuarios = (usuarios = []) => {
+  let usersHtml = '';
+  usuarios.forEach(({ nombre, uid }) => {
+    usersHtml += `
+      <li>
+        <p>
+          <h5 class="text-success">${nombre}</h5>
+          <span class="fs-6 text-muted">${uid}</span>
+        </p>
+      </li>
+    `;
+
+    ulUsuarios.innerHTML = usersHtml;
   });
 };
 
